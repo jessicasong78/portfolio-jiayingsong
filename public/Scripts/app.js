9740c9cb5457a5c761ac0e4b265e990df3e0ad04
Jiaying Song <jessicasong788@gmail.com>
2022-04-23 08:58:12 -0700

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,14 @@
+/*
+Student's Name: Jiaying Song
+StudentID: 301172953
+*/
+
+(function(){
+
+    function Start(){
+        console.log("function start");
+
+        
+    }
+    window.addEventListener("load",Start);
+})();
---
+++ routes/users.js
@@ -0,0 +1,13 @@
+/*
+Student's Name: Jiaying Song
+StudentID: 301172953
+*/
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('Placeholder');
+});
+
+module.exports = router;
